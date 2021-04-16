5e52c2a7263db83d205171b58b2657a79d5da67b
Piyush Panchal <77259664+ProgrammerPiyush008@users.noreply.github.com>
2021-04-16 12:41:55 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
 const doggoImageElement = document.querySelector(".doggo-img")
 const moreDoggoBtn = document.querySelector(".more-doggo-btn")
-const slideshowCheck = document.querySelector(".slideshow-check")
 const API_URL = "https://api.thedogapi.com/v1/images/search"
+const doggoBreed = document.querySelector(".breed")
 
 function setDoggo() {
   fetch(API_URL)
     .then((res) => res.json())
     .then((data) => {
       const doggo = data[0].url
+
+      if (data[0].breeds[0] === undefined) {
+        doggoBreed.textContent =
+          "Sorry I don't know this doggo. but he's cute! isnt he?"
+      } else {
+        doggoBreed.textContent = data[0].breeds[0].name
+      }
 
       doggoImageElement.setAttribute("src", doggo)
     })
