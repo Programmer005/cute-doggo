04b8cd009898e3d3ee85f4572343218c7173d3d3
Piyush Panchal <77259664+ProgrammerPiyush008@users.noreply.github.com>
2021-04-17 20:21:03 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const doggoImageElement = document.querySelector(".doggo-img")
 const moreDoggoBtn = document.querySelector(".more-doggo-btn")
 const API_URL = "https://api.thedogapi.com/v1/images/search"
 const doggoBreed = document.querySelector(".breed")
+const downloadPara = document.querySelector(".download-p")
 
 function setDoggo() {
   fetch(API_URL)
@@ -23,3 +24,9 @@ function setDoggo() {
 setDoggo()
 
 moreDoggoBtn.addEventListener("click", setDoggo)
+
+downloadPara.addEventListener("click", () => {
+  alert(
+    `Right click the doggo image, then click the "Save image as" button. Now you have a cute doggo with you.`
+  )
+})
